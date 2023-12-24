e4c3c622144c8f0683b6a89646e2fa9d694b0bc1
Daleth Darko <git@worthless.services>
2023-12-24 09:27:52 +0000

    Refactor FileUtils class and add StringUtils class
    The FileUtils class is refactored in this commit to improve its functionality. The `strip_non_ascii` method is renamed to `strip_non_ascii_filenames` for clarity. The method now renames files in the provided directory by removing non-ASCII characters. This helps to ensure that the renamed files don't overwrite existing files. The `StringUtils` class is also added, which provides a method `get_ascii_chars()` that returns a set of ASCII characters. This method is used in the refactored `strip_non_ascii_filenames` method to determine which characters should be considered ASCII. The commit also includes a version update in the setup.py file, from 0.1.1 to 0.1.2.

--- coffee/weneed/utils/file_utils.py
+++ coffee/weneed/utils/file_utils.py
@@ -1,45 +1,34 @@
 import os
-import string
-
+from string_utils import StringUtils
 class FileUtils:
     """
     A utility class for working with files.
     """
-    @staticmethod
-    def __strip_non_ascii_from_file(file_path, ascii_chars):
-        """
-        Internal method that reads a file, removes any non-ASCII characters,
-        and writes the clean content back to the file.
-        
-        Parameters:
-        file_path (str): The path to the file to be processed.
-        ascii_chars (set): Set of ASCII characters.
-        """
-        with open(file_path, 'r') as file:
-            contents = file.read()
 
-        ascii_contents = ''.join(c for c in contents if c in ascii_chars)
-        if contents != ascii_contents:
-            with open(file_path, 'w') as file:
-                file.write(ascii_contents)
+def strip_non_ascii_filenames(directory, file_extension=None, recursive=False):
+    """
+    Traverses through the provided directory and renames files in it by
+    removing non-ASCII characters, ensuring they don't overwrite an existing file.
 
-    @staticmethod
-    def strip_non_ascii(directory, file_extension=None, recursive=False):
-        """
-        Strips non-ASCII characters from all appropriate files in a given directory.
+    Parameters:
+    directory (str): Path to the directory to process.
+    file_extension (str, optional): Only files with this extension are processed. If None, all files are processed.
+    recursive (bool, optional): If True, subdirectories are processed recursively. Default is False.
+    """
+    ascii_chars = StringUtils.get_ascii_chars()
 
-        Parameters:
-        directory (str): The directory to be processed.
-        file_extension (str): An optional extension of the files to be processed. 
-                              All files will be processed if not specified. 
-        recursive (bool): A flag indicating whether to process files recursively in  
-                          all subdirectories (True), or only in the given directory (False). 
-                          Default is False.
-        """
-        ascii_chars = set(string.printable)
-        file_processing_method = FileUtils.__strip_non_ascii_from_file
+    for root, _, filenames in os.walk(directory) if recursive else [(directory, [], os.listdir(directory))]:
+        for filename in filenames:
+            if file_extension is None or filename.endswith(file_extension):
+                directory, old_filename = os.path.split(os.path.join(root, filename))
+                base, ext = os.path.splitext(old_filename)
+                new_base = ''.join(c for c in base if c in ascii_chars)
 
-        for root, _, filenames in os.walk(directory) if recursive else [(directory, [], os.listdir(directory))]:
-            for filename in filenames:
-                if file_extension is None or filename.endswith(file_extension):
-                    file_processing_method(os.path.join(root, filename), ascii_chars)
+                if base != new_base:
+                    new_filename = new_base
+                    counter = 1
+                    while os.path.exists(os.path.join(directory, new_filename + ext)):
+                        new_filename = new_base + "_" + str(counter)
+                        counter += 1
+
+                    os.rename(os.path.join(directory, old_filename), os.path.join(directory, new_filename + ext))
