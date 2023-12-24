1089ad89f453a536093488210b5798d641222b88
Daleth Darko <git@worthless.services>
2023-12-24 09:40:38 +0000

    Update version number in `setup.py` file.

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 from setuptools import setup, find_packages
 import os
 
-
-def get_version():
-    basedir = os.path.dirname(__file__)
-    with open(os.path.join(basedir, '.bumpversion.cfg')) as f:
-        version = [line.split('=')[1].strip() for line in f.readlines() if line.startswith('current_version = ')][0]
-    return version
-
-
 setup(
     name="coffee-utils",
-    version=get_version(),
+    version="0.1.3",
     packages=find_packages(),
 )
