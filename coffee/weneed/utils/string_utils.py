22daab5f31aa90cbd9f03bcabfd1f99998f814f2
Daleth Darko <git@worthless.services>
2024-02-21 14:01:41 +0000

    weneed/utils: Modify graphic_as_string method in StringUtils
    The graphic_as_string method in the StringUtils class has been modified. The width argument has been changed from 49 to 45. The logic for generating the graphic string has also been updated to use different border characters.

--- coffee/weneed/utils/string_utils.py
+++ coffee/weneed/utils/string_utils.py
@@ -1,19 +1,18 @@
 import string
 
-
 class StringUtils:
 
     @staticmethod
-    def graphic_as_string(texts, width=49):
+    def graphic_as_string(texts, width=45):
         if isinstance(texts, str):
             texts = [texts]
-        border = '=' * width
+        border = '>' * (width//2) + ('*' if width % 2 else '') + '<' * (width//2)
         graphic_string = border + "\n"
 
         for text in texts:
             empty_space = ' ' * ((width - len(text) - 4) // 2)
-            graphic_string += '== ' + empty_space + text + empty_space + (
-                '==' if len(empty_space) * 2 + len(text) + 4 == width else '===') + "\n"
+            graphic_string += '||' + empty_space + text + empty_space + (
+                ' ||' if len(empty_space) * 2 + len(text) + 5 == width else '||') + "\n"
 
         graphic_string += border
         return graphic_string
