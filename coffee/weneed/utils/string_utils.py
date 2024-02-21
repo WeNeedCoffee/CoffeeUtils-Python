5031d506ff9ec911d5168c4a893a7d2e87ff4484
Daleth Darko <git@worthless.services>
2024-02-21 13:39:44 +0000

    [weneed.utils.string_utils]: Refactor function `print_graphic` to `graphic_as_string` in StringUtils class
    Refactor the function `print_graphic` in the `StringUtils` class to `graphic_as_string`. This function now accepts a list of texts instead of a single text. The function generates a graphic string by adding borders and formatting the texts. The width of the graphic string is controlled by the `width` parameter. The generated graphic string is returned as the result of this function.

--- coffee/weneed/utils/string_utils.py
+++ coffee/weneed/utils/string_utils.py
@@ -4,12 +4,19 @@ import string
 class StringUtils:
 
     @staticmethod
-    def print_graphic(text, width=49):
+    def graphic_as_string(texts, width=49):
+        if isinstance(texts, str):
+            texts = [texts]
         border = '=' * width
-        empty_space = ' ' * ((width - len(text) - 4) // 2)
-        print(border)
-        print('==', empty_space, text, empty_space, '==' if len(empty_space) * 2 + len(text) + 4 == width else '===')
-        print(border)
+        graphic_string = border + "\n"
+
+        for text in texts:
+            empty_space = ' ' * ((width - len(text) - 4) // 2)
+            graphic_string += '== ' + empty_space + text + empty_space + (
+                '==' if len(empty_space) * 2 + len(text) + 4 == width else '===') + "\n"
+
+        graphic_string += border
+        return graphic_string
 
     @staticmethod
     def get_ascii_chars():
